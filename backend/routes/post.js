d04ea2ba9c062fc80233ce630a7899dff8df943d
Sayaka Vincent <sfv2010@hotmail.fr>
2022-08-22 11:05:46 +0200

    add timeline

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,12 +1,14 @@
-//const express = require("express");
 const router = require("express").Router();
 const postCtrl = require("../controllers/post");
 const auth = require("../middleware/auth");
+//const multer = require("../middleware/multer-config");
 
 router.post("/", auth, postCtrl.createPost);
 router.put("/:id", auth, postCtrl.updatePost);
 router.delete("/:id", auth, postCtrl.deletePost);
+router.get("/", auth, postCtrl.getAllPost);
 router.get("/:id", auth, postCtrl.getOnePost);
 router.post("/:id/like", auth, postCtrl.likerPost);
+router.get("/timeline/all", auth, postCtrl.timelinePost);
 
 module.exports = router;
