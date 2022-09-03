0b212e7fb76b4844cda796839f59d93723cd5db7
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-03 23:04:53 +0200

    add multer

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const postCtrl = require("../controllers/post");
 const auth = require("../middleware/auth");
-//const multer = require("../middleware/multer-config");
+const multer = require("../middleware/multer-config");
 
-router.post("/", auth, postCtrl.createPost);
-router.put("/:id", auth, postCtrl.updatePost);
-router.delete("/:id", auth, postCtrl.deletePost);
+router.post("/", auth, multer, postCtrl.createPost);
+router.put("/:id", auth, multer, postCtrl.updatePost);
+router.delete("/:id", auth, multer, postCtrl.deletePost);
 router.get("/", auth, postCtrl.getAllPost);
 router.get("/:id", auth, postCtrl.getOnePost);
 router.post("/:id/like", auth, postCtrl.likerPost);
