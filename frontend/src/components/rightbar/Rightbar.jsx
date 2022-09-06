4620a8585e3c1a30b1752a1405fdc15b63321770
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-06 12:04:00 +0200

    add tags

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 import "./Rightbar.css";
+import { Users } from "../../dummydata";
+import { Online } from "../online/Online";
 
 export const Rightbar = () => {
-    return <div className="rightbar">Rightbar</div>;
+    return (
+        <section className="rightbar">
+            <div className="rightbarWrapper">
+                <div className="eventContainer">
+                    <img
+                        src="/assets/star.png"
+                        alt="etoile bleu"
+                        className="starImg"
+                    />
+                    <h2 className="eventText">
+                        <b>Nouvelles de Groupomania</b>
+                    </h2>
+                </div>
+                <img
+                    src="/assets/post/equipe.jpg"
+                    alt="4 nouvelles recrues"
+                    className="rightbarImg"
+                />
+                <h3 className="rightbarTitle">Amis en ligne</h3>
+                <ul className="rightbarFriendList">
+                    {Users.map((user) => (
+                        <Online user={user} key={user.id} />
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
 };
