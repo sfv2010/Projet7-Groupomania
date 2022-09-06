b3f529dc8a94351e76d3700514cc0ef33841dc19
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-06 12:07:26 +0200

    add import Posts

--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Share } from "../share/Share";
 import { Post } from "../post/Post";
 import "./Timeline.css";
+import { Posts } from "../../dummydata";
 
 export const Timeline = () => {
     return (
         <div className="timeline">
             <div className="timelineWrapper"></div>
             <Share />
-            <Post />
+
+            {Posts.map((post) => (
+                <Post post={post} key={post.id} />
+            ))}
         </div>
     );
 };
