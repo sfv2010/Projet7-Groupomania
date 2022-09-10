884dea53f16105d1567aec8e7b4939ae4fbd32b1
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-10 18:02:41 +0200

    Add Link

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -2,37 +2,40 @@ import {
     AccountCircle,
     Home,
     Notifications,
+    PowerSettingsNew,
     Search,
 } from "@mui/icons-material";
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Topbar.css";
 
 export const Topbar = () => {
+    // const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
     return (
         <header className="topbarContainer">
             <h1 className="topbarLeft">
                 <img
-                    src="/assets/Groupomania /icon-left-font-monochrome-white.svg"
+                    src="/assets/groupomania /icon-left-font-monochrome-white.svg"
                     alt="Logo de Groupomania"
                     className="topbarLogo"
                 />
             </h1>
             <ul className="topbarCenter">
-                <li>
-                    <a aria-label="Accueil" href="index.html">
+                <Link to="/">
+                    <li className="topbarList" aria-label="Accueil">
                         <Home className="topbarIcon" />
-                    </a>
-                </li>
-                <li>
-                    <a aria-label="Compte" href="index.html">
+                        <span className="topbarNotice">Accueil</span>
+                    </li>
+                </Link>
+                <Link to="/profile/:username">
+                    <li className="topbarList" aria-label="Profil">
                         <AccountCircle className="topbarIcon" />
-                    </a>
-                </li>
-                <li>
-                    <a aria-label="Notification" href="index.html">
-                        <Notifications className="topbarIcon" />
-                        <span className="topbarIconBadge">1</span>
-                    </a>
+                        <span className="topbarNotice">Profil</span>
+                    </li>
+                </Link>
+                <li className="topbarList" ria-label="Notification">
+                    <Notifications className="topbarIcon" />
+                    <span className="topbarNotice">Notification</span>
                 </li>
             </ul>
             <div className="topbarRight">
@@ -44,11 +47,10 @@ export const Topbar = () => {
                         placeholder="Rechercher sur Groupomania"
                     />
                 </div>
-                <img
-                    src="/assets/person/cheval.jpg"
-                    alt="Icon de cheval"
-                    className="topbarImg"
-                />
+                <div className="topbarLogout">
+                    <PowerSettingsNew className="topbarIcon logout" />
+                    <span className="topbarNotice">Quitter</span>
+                </div>
             </div>
         </header>
     );
