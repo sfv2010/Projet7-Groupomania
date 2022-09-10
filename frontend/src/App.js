4e54b207cc6e6202b7e6d8df6949d0e8abbf8272
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-10 17:56:58 +0200

    Add BrouserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
+import React from "react";
 import Home from "./pages/home/Home";
+import { Profile } from "./pages/profile/Profile";
+import { Register } from "./pages/register/Register";
+import { Login } from "./pages/login/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export const App = () => {
-    return <Home />;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/profile/:username" element={<Profile />} />
+            </Routes>
+        </Router>
+    );
 };
-
+// export const App = () => {
+//     return <Login />;
+// };
 export default App;
