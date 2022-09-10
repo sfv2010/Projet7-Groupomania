b197fdeff8a999e12336763eefa821103be61777
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-10 18:03:23 +0200

    add useRef

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,34 +1,50 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Login.css";
 
 export const Login = () => {
+    const email = useRef();
+    const password = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //pour ne pas reload
+        console.log(email.current.value);
+        console.log(password.current.value);
+    };
+
     return (
         <div className="login">
             <div className="loginWrapper">
                 <header className="loginLeft">
                     <h1>
                         <img
-                            src="/assets/Groupomania /icon-left-font-monochrome-white.svg"
+                            src="/assets/groupomania /icon-left-font-monochrome-white.svg"
                             alt=" Grand logo de Groupomania"
                             className="loginLogo"
                         />
                     </h1>
                     <p className="loginDescription">
-                        Réseau social d'entreprise de Groupomania
+                        Travaillons plus efficacement, ensemble
                     </p>
                 </header>
                 <div className="loginRight">
-                    <div className="loginBox">
+                    <form
+                        className="loginBox"
+                        onSubmit={(e) => handleSubmit(e)}
+                    >
                         <p className="loginMsg">Connexion</p>
                         <input
-                            type="text"
+                            type="email"
                             className="loginInput"
                             placeholder="Adresse e-mail"
+                            required
+                            ref={email}
                         />
                         <input
-                            type="text"
+                            type="password"
                             className="loginInput"
                             placeholder="Mot de passe"
+                            required
+                            ref={password}
                         />
 
                         <button className="loginButton">Se connecter</button>
@@ -38,7 +54,7 @@ export const Login = () => {
                         <button className="loginRegister">
                             Créer nouveaux compte
                         </button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
