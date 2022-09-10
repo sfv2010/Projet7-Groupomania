1c92a6dd404ed7fa3c5fe1ce975872593d624829
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-10 17:57:22 +0200

    add dummy data

--- frontend/src/dummydata.js
+++ frontend/src/dummydata.js
@@ -54,21 +54,4 @@ export const Posts = [
         like: 230,
         comment: 29,
     },
-    {
-        id: 4,
-        photo: "assets/post/4.jpeg",
-        date: "1分前",
-        userId: 4,
-        like: 10,
-        comment: 1,
-    },
-    {
-        id: 5,
-        desc: "定期的に散歩した方が生産性が上がる。",
-        photo: "assets/post/5.jpeg",
-        date: "1分前",
-        userId: 5,
-        like: 100,
-        comment: 3,
-    },
 ];
