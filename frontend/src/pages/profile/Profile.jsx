527b7d67528d6dce79b2e509aaa96f5685526461
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-10 18:04:01 +0200

    Add Profile

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,40 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Profile.css";
 import { Topbar } from "../../components/topbar/Topbar";
-import { Sidebar } from "../../components/sidebar/Sidebar";
 import { Timeline } from "../../components/timeline/Timeline";
 import { Rightbar } from "../../components/rightbar/Rightbar";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export const Profile = () => {
+    const [user, setUser] = useState({});
+    const username = useParams().username;
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            // const res = await axios.get(`users/${post.userId}`);
+            //クエリの取得ができてない。
+            // const res = await axios.get("/users?username=shincode");
+            // const res = await axios.get(`/users?username=shincode`);
+            const res = await axios.get(`/users?username=${username}`);
+
+            setUser(res.data);
+            // console.log(res.data);
+        };
+        fetchUser();
+    }, [username]); //普通は空箱！
+
     return (
         <>
             <Topbar />
             <div className="profileContainer">
-                <Sidebar />
                 <div className="profile">
                     <div className="profileTop">
                         <div className="profileCover">
                             <img
-                                src="/assets/post/paysage.jpg"
+                                src={
+                                    user.coverPicture ||
+                                    "/assets/post/paysage.jpg"
+                                }
                                 alt="paysage de campagne"
                                 className="profileImg"
                             />
                             <img
-                                src="/assets/person/cheval.jpg"
-                                alt="icon de cheval"
+                                src={
+                                    user.profilePicture ||
+                                    "/assets/person/icon.png"
+                                }
+                                alt="L'utilisateur n' a pas ajouter d' icon"
                                 className="profileUserImg"
                             />
                         </div>
                         <div className="profileInfo">
-                            <h2 className="profileInfoName">Savin</h2>
+                            <h2 className="profileInfoName">{user.username}</h2>
                             <span className="profileInfoDesc">
-                                J'apprends React tous les jours!{" "}
+                                {user.description}
                             </span>
                         </div>
                     </div>
                     <div className="profileBottom">
-                        <Timeline />
-                        <Rightbar profile />
+                        <Timeline username={username} />
+                        <Rightbar user={user} />
                     </div>
                 </div>
             </div>
