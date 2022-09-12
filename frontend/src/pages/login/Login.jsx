d8575dd8fe5e125562b493def717588c0165cf14
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-12 02:35:17 +0200

    add loginCall

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,15 +1,25 @@
-import React, { useRef } from "react";
+import React, { useContext, useRef } from "react";
+import { loginCall } from "../../state/dispatch";
+import { AuthContext } from "../../state/AuthContext";
 import "./Login.css";
 
 export const Login = () => {
     const email = useRef();
     const password = useRef();
+    const { user, isFetching, error, dispatch } = useContext(AuthContext);
 
     const handleSubmit = (e) => {
         e.preventDefault(); //pour ne pas reload
-        console.log(email.current.value);
-        console.log(password.current.value);
+        loginCall(
+            {
+                email: email.current.value,
+                password: password.current.value,
+            },
+            dispatch
+        );
     };
+
+    console.log(user);
 
     return (
         <div className="login">
@@ -60,3 +70,76 @@ export const Login = () => {
         </div>
     );
 };
+
+// import React, { useContext, useRef } from "react";
+// import { loginCall } from "../../dispatch";
+// import { AuthContext } from "../../state/AuthContext";
+// import "./Login.css";
+
+// export const Login = () => {
+//     const email = useRef();
+//     const password = useRef();
+//     const { user, dispatch } = useContext(AuthContext);
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault(); //pour ne pas reload
+//         loginCall(
+//             {
+//                 email: email.current.value,
+//                 password: password.current.value,
+//             },
+//             dispatch
+//         );
+//     };
+
+//     console.log(user);
+
+//     return (
+//         <div className="login">
+//             <div className="loginWrapper">
+//                 <header className="loginLeft">
+//                     <h1>
+//                         <img
+//                             src="/assets/groupomania /icon-left-font-monochrome-white.svg"
+//                             alt=" Grand logo de Groupomania"
+//                             className="loginLogo"
+//                         />
+//                     </h1>
+//                     <p className="loginDescription">
+//                         Travaillons plus efficacement, ensemble
+//                     </p>
+//                 </header>
+//                 <div className="loginRight">
+//                     <form
+//                         className="loginBox"
+//                         onSubmit={(e) => handleSubmit(e)}
+//                     >
+//                         <p className="loginMsg">Connexion</p>
+//                         <input
+//                             type="email"
+//                             className="loginInput"
+//                             placeholder="Adresse e-mail"
+//                             required
+//                             ref={email}
+//                         />
+//                         <input
+//                             type="password"
+//                             className="loginInput"
+//                             placeholder="Mot de passe"
+//                             required
+//                             ref={password}
+//                         />
+
+//                         <button className="loginButton">Se connecter</button>
+//                         <p className="loginForget">Mot de passe oublié?</p>
+//                         <hr />
+//                         <p className="loginFailed">Pas de compte?</p>
+//                         <button className="loginRegister">
+//                             Créer nouveaux compte
+//                         </button>
+//                     </form>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
