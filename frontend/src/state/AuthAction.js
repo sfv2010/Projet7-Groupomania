99b6b83ff4f256868d2a938128b9970781104e05
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-12 19:12:56 +0200

    change the comments

--- frontend/src/state/AuthAction.js
+++ frontend/src/state/AuthAction.js
@@ -1,10 +1,10 @@
-//ユーザー入力に応じたアクションの設定
+//Définition d'actions basées sur l'entrée de l'utilisateur
 export const LoginStart = (user) => ({
     type: "LOGIN_START",
 });
 export const LoginSuccess = (user) => ({
     type: "LOGIN_SUCCESS",
-    payload: user,
+    payload: user, //payload（データ本体）は任意につけた名前。
 });
 export const LoginError = (error) => ({
     type: "LOGIN_ERROR",
