091b490cb0764f0ef065da0d61d89874c2d1048a
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-12 19:10:13 +0200

    add AuthContext

--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useContext, useRef, useState } from "react";
 import "./Share.css";
 import { AddAPhoto, GifBox } from "@mui/icons-material";
+import { AuthContext } from "../../state/AuthContext";
+import axios from "axios";
 
 export const Share = () => {
+    const { user } = useContext(AuthContext);
+    const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+    const desc = useRef();
+
+    const [file, setFile] = useState(null);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newPost = {
+            userId: user._id,
+            desc: desc.current.value,
+        };
+        if (file) {
+            const data = new FormData();
+            const fileName = Date.now() + file.name;
+            data.append("name", fileName);
+            data.append("file", file);
+            newPost.img = fileName;
+            console.log(newPost);
+            try {
+                await axios.post("/upload", data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        try {
+            await axios.post("/posts", newPost);
+            window.location.reload();
+        } catch (err) {
+            console.log(err);
+        }
+    };
     return (
         <main className="share">
             <div className="shareWrapper">
                 <div className="shareTop">
                     <img
-                        src="/assets/person/cheval.jpg"
+                        src={
+                            user.profilePicture
+                                ? PUBLIC_FOLDER + user.profilePicture
+                                : //: PUBLIC_FOLDER + "person/Anonym.svg"
+                                  "/assets/person/Anonym.svg"
+                        }
                         alt="icon de User"
                         className="shareProfileImg"
                     />
@@ -17,23 +55,36 @@ export const Share = () => {
                             type="text"
                             className="shareInput"
                             placeholder="Quoi de neuf?"
+                            ref={desc}
                         />
                     </div>
                 </div>
                 <hr className="shareHr" />
-                <div className="shareButtons">
+                <form
+                    className="shareButtons"
+                    onSubmit={(e) => handleSubmit(e)}
+                    encType="multipart/form-data"
+                >
                     <div className="shareOptions">
-                        <div className="shareOption">
+                        <label htmlFor="file" className="shareOption">
                             <AddAPhoto className="shareIcon" htmlColor="blue" />
                             <span className="shareOptionText">Photo</span>
-                        </div>
+                            <input
+                                style={{ display: "none" }}
+                                type="file"
+                                id="file"
+                                accept=".png, .jpeg, .jpg"
+                                onChange={(e) => setFile(e.target.files[0])}
+                                name="file"
+                            />
+                        </label>
                         <div className="shareOption">
                             <GifBox className="shareIcon" htmlColor="red" />
                             <span className="shareOptionText">GIF</span>
                         </div>
                     </div>
                     <button className="shareButton">Publier</button>
-                </div>
+                </form>
             </div>
         </main>
     );
