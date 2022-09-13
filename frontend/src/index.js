0665bb04d39f93ff571c1e7b959f8eeb88246d96
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-13 12:58:28 +0200

    remove the comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,20 +5,9 @@ import { AuthContextProvider } from "./state/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <AuthContextProvider>
-            <App />
-        </AuthContextProvider>
-    </React.StrictMode>
+    // <React.StrictMode>
+    <AuthContextProvider>
+        <App />
+    </AuthContextProvider>
+    // </React.StrictMode>
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>
-// );
