73740030fb26e34e79037be0711a0e22512dcdf8
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-14 10:08:26 +0200

    change the name description

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema(
     {
         userId: { type: String, required: true },
-        description: { type: String, max: 200 },
+        desc: { type: String, max: 200 },
         img: { type: String, default: "" },
         likes: { type: Array, default: [] },
     },
