e97f880bcf14c2c6b7d1445336fa48c472be4fa1
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-14 16:04:19 +0200

    fix response

--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -3,35 +3,23 @@ import { Share } from "../share/Share";
 import { Post } from "../post/Post";
 import "./Timeline.css";
 import axios from "axios";
-
 import { AuthContext } from "../../state/AuthContext";
 
 export const Timeline = ({ username }) => {
     const [posts, setPosts] = useState([]);
     const { user } = useContext(AuthContext);
 
-    // useEffect(() => {
-    //     const fetchPosts = async () => {
-    //         const response = await axios.get(
-    //             `http://localhost:4000/api/posts/timeline/${user._id}`
-    //         );
-    //         console.log(response);
-    //         setPosts(response.data);
-    //     };
-    //     fetchPosts();
-    // }, [user]); //,[]=juste premier fois
-
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = username
+            const res = username
                 ? await axios.get(
                       `http://localhost:4000/api/posts/profile/${username}`
                   )
                 : await axios.get(
                       `http://localhost:4000/api/posts/timeline/${user._id}`
                   );
-            console.log(response);
-            setPosts(response.data);
+            console.log(res);
+            setPosts(res.data);
         };
         fetchPosts();
     }, [username, user._id]); //,[]=juste premier fois
