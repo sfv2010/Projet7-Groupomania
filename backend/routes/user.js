100b34e77646d9aa9efab426eed9acab3505b0d4
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-15 15:54:33 +0200

    remove getAllUser

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,7 +5,6 @@ const auth = require("../middleware/auth");
 
 router.put("/:id", auth, userCtrl.updateUser);
 router.delete("/:id", auth, userCtrl.deleteUser);
-router.get("/", auth, userCtrl.getAllUser);
 router.get("/", auth, userCtrl.getOneUser);
 router.put("/:id/follow", auth, userCtrl.followUser);
 router.put("/:id/unfollow", auth, userCtrl.unFollowUser);
