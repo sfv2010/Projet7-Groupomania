6261eb6c007b1760a4031167981b21f3a41b28cf
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-16 12:36:56 +0200

    add header Authorization

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,28 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./Profile.css";
 import { Topbar } from "../../components/topbar/Topbar";
 import { Timeline } from "../../components/timeline/Timeline";
 import { Rightbar } from "../../components/rightbar/Rightbar";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { AuthContext } from "../../state/AuthContext";
 
 export const Profile = () => {
     const [user, setUser] = useState({});
     const username = useParams().username;
     const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+    const { user: currentUser } = useContext(AuthContext);
 
     useEffect(() => {
         const fetchUser = async () => {
             //Rechercher aprés ? sur url
             const res = await axios.get(
-                `http://localhost:4000/api/users?username=${username}`
+                `http://localhost:4000/api/users?username=${username}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${currentUser.token}`,
+                    },
+                }
             );
 
             setUser(res.data);
             // console.log(res.data);
         };
         fetchUser();
-    }, [username]); //普通は空箱！
+    }, [username, currentUser.token]); //普通は空箱！
 
     return (
         <>
@@ -33,16 +40,18 @@ export const Profile = () => {
                         <div className="profileCover">
                             <img
                                 src={
-                                    PUBLIC_FOLDER + user.coverPicture ||
-                                    PUBLIC_FOLDER + "/post/paysage.jpg"
+                                    user.coverPicture
+                                        ? PUBLIC_FOLDER + user.coverPicture
+                                        : PUBLIC_FOLDER + "/post/paysage.jpg"
                                 }
-                                alt="paysage de campagne"
+                                alt="arrière-plan de l'utilisateur"
                                 className="profileImg"
                             />
                             <img
                                 src={
-                                    PUBLIC_FOLDER + user.profilePicture ||
-                                    PUBLIC_FOLDER + "/person/Anonym.svg"
+                                    user.profilePicture
+                                        ? PUBLIC_FOLDER + user.profilePicture
+                                        : PUBLIC_FOLDER + "/person/Anonym.svg"
                                 }
                                 alt="L'utilisateur n'a pas ajouter d'icon"
                                 className="profileUserImg"
