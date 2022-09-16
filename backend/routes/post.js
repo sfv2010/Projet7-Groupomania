5e7e721416debd40f53a98f4bf18ba52a8080475
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-16 12:34:20 +0200

    fix router like

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -8,7 +8,7 @@ router.put("/:id", auth, multer, postCtrl.updatePost);
 router.delete("/:id", auth, multer, postCtrl.deletePost);
 router.get("/", auth, postCtrl.getAllPost);
 router.get("/:id", auth, postCtrl.getOnePost);
-router.post("/:id/like", auth, postCtrl.likerPost);
+router.put("/:id/like", auth, postCtrl.likerPost);
 router.get("/profile/:username", auth, postCtrl.prpfilePost);
 router.get("/timeline/:userId", auth, postCtrl.timelinePost);
 
