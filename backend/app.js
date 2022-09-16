b281f990dee4fe5855fc1132e657cc09789f98ff
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-16 22:50:36 +0200

    add express.urlencoded

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ mongoose
 
 // Pour gérer la requête POST venant de l'application front-end, on a besoin d'en extraire le corps JSON.
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //--- CORS---
 app.use(cors());
