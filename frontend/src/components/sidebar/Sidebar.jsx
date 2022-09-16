ca2f93f48a67600a5720038d4423aa5502bfacf7
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-16 15:34:34 +0200

    add AuthContext

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,33 @@
 import { Home, MessageRounded, Person, Search } from "@mui/icons-material";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ListFriend } from "../listFriend/ListFriend";
 import "./Sidebar.css";
 import { Users } from "../../dummydata";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from "../../state/AuthContext";
 
 export const Sidebar = () => {
+    const { user: currentUser } = useContext(AuthContext);
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            //Rechercher aprés ? sur url
+            const res = await axios.get(
+                `http://localhost:4000/api/users?userId=${currentUser.userId}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${currentUser.token}`,
+                    },
+                }
+            );
+
+            setUser(res.data);
+            // console.log(res.data);
+        };
+        fetchUser();
+    }, [currentUser]); //普通は空箱！
     return (
         <nav className="sidebar">
             <div className="sidebarWrapper">
@@ -24,7 +46,7 @@ export const Sidebar = () => {
                     </li>
                     <li className="sidebarListItem">
                         <Person className="sidebarIcon" />
-                        <Link to="/profile/:username">
+                        <Link to={`/profile/${user.username}`}>
                             <span className="sidebarText">Mon Compte</span>
                         </Link>
                     </li>
