66ef9d7f8bdda450405ba156cab3f2975683bdbf
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-17 15:50:17 +0200

    delete post

--- frontend/src/dummydata.js
+++ frontend/src/dummydata.js
@@ -25,33 +25,3 @@ export const Users = [
         username: "Sena",
     },
 ];
-
-export const Posts = [
-    {
-        id: 1,
-        desc: "J'apprends React",
-        photo: "/assets/post/study.jpg",
-        date: "5分前",
-        userId: 1,
-        like: 20,
-        comment: 4,
-    },
-    {
-        id: 2,
-        desc: "MERNスタックが楽しすぎる",
-        photo: "/assets/post/chevaux.jpg",
-        date: "2分前",
-        userId: 2,
-        like: 140,
-        comment: 13,
-    },
-    {
-        id: 3,
-        desc: "ShinCodeさんの解説が分かりやすい。",
-        photo: "/assets/post/equipe.jpg",
-        date: "10分前",
-        userId: 3,
-        like: 230,
-        comment: 29,
-    },
-];
