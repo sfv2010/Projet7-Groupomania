bc9b7e9580cc4d259aa2ddd5602cfb9f2b0ffc30
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-17 15:51:22 +0200

    add css

--- frontend/src/components/listFriend/ListFriend.jsx
+++ frontend/src/components/listFriend/ListFriend.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./ListFriend.css";
 
 export const ListFriend = ({ user }) => {
+    //const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
     return (
         <>
             <li className="sidebarFriend">
                 <img
+                    // src={PUBLIC_FOLDER + user.profilePicture}
                     src={user.profilePicture}
                     alt="icon d'ami"
                     className="sidebarFriendImg"
