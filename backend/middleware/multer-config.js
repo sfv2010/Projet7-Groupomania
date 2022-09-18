1a9e697ed3d1db19761a848b3e0a4dc9106fa285
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-18 23:53:42 +0200

    delete the comment

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -19,11 +19,8 @@ const storage = multer.diskStorage({
         cb(null, "images");
     },
     filename: (req, file, cb) => {
-        //---la fonction filename indique à multer d'utiliser le nom d'origine, de remplacer les espaces par des underscores et d'ajouter un timestamp Date.now() comme nom de fichier.
-        //const name = file.originalname.split(" ").join("_"); // le nom d'origine en eliminant espace et on donne _ à la place d'espace sinon on l'image ne passera pas
         const extension = MIME_TYPES[file.mimetype]; //la constante dictionnaire de type MIME pour résoudre l'extension de fichier appropriée.
         cb(null, req.body.name);
-        //cb(null, req.body.name + Date.now() + "." + extension); //d'ajouter un timestamp Date.now() comme nom de fichier pour différencier s'il y a des même nom de fichier.
     },
 });
 
