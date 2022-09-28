51dbff4d1630e9e319a751506c8d0ed363a5fb10
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-28 02:32:25 +0200

    Remove the comment

--- frontend/src/components/listFriend/ListFriend.jsx
+++ frontend/src/components/listFriend/ListFriend.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import "./ListFriend.css";
 
 export const ListFriend = ({ user }) => {
-    //const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
     return (
         <>
             <li className="sidebarFriend">
                 <img
-                    // src={PUBLIC_FOLDER + user.profilePicture}
                     src={user.profilePicture}
                     alt="icon d'ami"
                     className="sidebarFriendImg"
