ef8c27fbddc59ad83003da03b4d88d9bceda6c8f
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-28 02:30:26 +0200

    Add multer

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const commentCtrl = require("../controllers/comment");
 const auth = require("../middleware/auth");
-//const multer = require("../middleware/multer-config");
+const multer = require("../middleware/multer-config");
 
-router.post("/:postId", auth, commentCtrl.createComment);
-router.put("/:id", auth, commentCtrl.updateComment);
-router.delete("/:id", auth, commentCtrl.deleteComment);
+router.post("/:postId", auth, multer, commentCtrl.createComment);
+router.put("/:id", auth, multer, commentCtrl.updateComment);
+router.delete("/:id", auth, multer, commentCtrl.deleteComment);
 router.get("/", auth, commentCtrl.getAllComment);
 router.get("/:id", auth, commentCtrl.getOneComment);
 
