7ccd9ccd7a28a047999aa34c9f1691a9cd9a1cb1
Sayaka Vincent <sfv2010@hotmail.fr>
2022-09-30 00:20:14 +0200

    Fix handleSubmit

--- frontend/src/components/comment/CommentShare.jsx
+++ frontend/src/components/comment/CommentShare.jsx
@@ -6,15 +6,10 @@ import "./CommentShare.css";
 export const CommentShare = ({ post, user, currentUser }) => {
     const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
     const desc = useRef();
-    const [validPost, setValidPost] = useState("");
     const [file, setFile] = useState(null);
-    const [commentText, setCommentText] = useState("");
+    const [validPost, setValidPost] = useState("");
 
-    console.log(post);
-    console.log(user);
-    console.log(currentUser);
-
-    const handleClick = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         const newComment = {
@@ -22,7 +17,6 @@ export const CommentShare = ({ post, user, currentUser }) => {
             commentDesc: desc.current.value,
             postId: post._id,
         };
-        console.log(desc);
         if (file) {
             const data = new FormData(); //fonction qui peut avoir la data avec "key + value"
             const fileName = Date.now() + file.name;
@@ -59,81 +53,66 @@ export const CommentShare = ({ post, user, currentUser }) => {
             console.log(err);
         }
     };
-    const onChangeComment = (e) => {
-        setCommentText(e.target.value);
-    };
 
     return (
-        <div className="shareWrapper">
-            <hr className="commentHr" />
-            <div className="shareTop">
-                <img
-                    src={
-                        user.profilePicture
-                            ? PUBLIC_FOLDER + user.profilePicture
-                            : PUBLIC_FOLDER + "/person/Anonym.svg"
-                    }
-                    alt="icon de User"
-                    className="shareProfileImg"
-                />
+        <main className="share">
+            <div className="commentShare">
+                <hr className="commentHr" />
+                <div className="shareTop">
+                    <img
+                        src={
+                            user.profilePicture
+                                ? PUBLIC_FOLDER + user.profilePicture
+                                : PUBLIC_FOLDER + "/person/Anonym.svg"
+                        }
+                        alt="icon de User"
+                        className="shareProfileImg"
+                    />
 
-                <div className="sharePost">
-                    <textarea
-                        value={commentText}
-                        onChange={onChangeComment}
-                        type="text"
-                        className="shareInput"
-                        placeholder="Écrivez un commentaire"
-                        ref={desc}
-                    ></textarea>
-                </div>
-            </div>
-            <span className="shareValidPost">{validPost}</span>
-            <hr className="shareHr" />
-            <form
-                className="shareButtons"
-                //onSubmit={(e) => handleSubmit(e)}
-                encType="multipart/form-data"
-            >
-                <div className="shareOptions">
-                    <label htmlFor="file" className="shareOption">
-                        <AddAPhoto className="shareIcon" htmlColor="blue" />
-                        <span className="shareOptionText">Photo</span>
-                        <input
-                            className="shareInputImg"
-                            type="file"
-                            id="file"
-                            accept=".png, .jpeg, .jpg"
-                            onChange={(e) => setFile(e.target.files[0])}
-                            name="file"
-                        />
-                    </label>
-                    <div className="shareOption">
-                        <GifBox className="shareIcon" htmlColor="red" />
-                        <span className="shareOptionText">GIF</span>
+                    <div className="sharePost">
+                        <textarea
+                            type="text"
+                            className="shareInput"
+                            placeholder="Écrivez un commentaire"
+                            ref={desc}
+                        ></textarea>
                     </div>
                 </div>
-                <button
-                    className="shareButton"
-                    type="text"
-                    onClick={handleClick}
+                <span className="shareValidPost">{validPost}</span>
+                <hr className="shareHr commentHr" />
+                <form
+                    className="shareButtons"
+                    onSubmit={(e) => handleSubmit(e)}
+                    encType="multipart/form-data"
                 >
-                    Publier
-                </button>
-            </form>
-
-            <div className="commentWrapper">
-                {/* {post.comments.map((commentItem, index) => {
-                    return (
-                        <div key={index} className="commentMap">
-                            <div>{commentItem.desc}</div>
+                    <div className="shareOptions commentOptions">
+                        <div className="shareOption">
+                            <label className="shareOption">
+                                <AddAPhoto
+                                    className="shareIcon"
+                                    htmlColor="blue"
+                                />
+                                <span className="shareOptionText">Photo</span>
+                                <input
+                                    className="shareInputImg"
+                                    type="file"
+                                    accept=".png, .jpeg, .jpg"
+                                    onChange={(e) => setFile(e.target.files[0])}
+                                    name="file"
+                                />
+                            </label>
                         </div>
-                    );
-                })} */}
-                {/* {post.comment.map((commentItem, index) => (
-                    <Comment commentItem={commentItem} key={index} />
-                ))} */}
+
+                        <div className="shareOption">
+                            <GifBox className="shareIcon" htmlColor="red" />
+                            <span className="shareOptionText">GIF</span>
+                        </div>
+                    </div>
+                    <button className="shareButton" type="submit">
+                        Publier
+                    </button>
+                </form>
             </div>
-        </div>
+        </main>
     );
 };
