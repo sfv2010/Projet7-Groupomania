b7b5652ff3462000abcd20df836c32d5dc7664c7
Sayaka Vincent <sfv2010@hotmail.fr>
2022-10-01 00:56:51 +0200

    Add multer

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/user");
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
-router.put("/:id", auth, userCtrl.updateUser);
+router.put("/:id", auth, multer, userCtrl.updateUser);
 router.delete("/:id", auth, userCtrl.deleteUser);
 router.get("/", auth, userCtrl.getOneUser);
 // router.put("/:id/follow", auth, userCtrl.followUser);
